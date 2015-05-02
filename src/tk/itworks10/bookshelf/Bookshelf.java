daf74253660a517ed7fefadd50cca86f5e9ca831
Itworks10 <Alexandre.V.Leger@gmail.com>
2015-05-02 15:55:28 +0200

    Add main class.

---
+++ src/tk/itworks10/bookshelf/Bookshelf.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2015, Itworks10 <Alexandre.V.Leger@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package tk.itworks10.bookshelf;
+
+import tk.itworks10.bookshelf.controller.Application;
+
+/**
+ * Main class of the project.
+ *
+ * @author Itworks10
+ */
+public class Bookshelf {
+
+    /**
+     * Start the program.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Application app;
+        try {
+            app = new Application();
+            app.run();
+        } catch (Exception e) {
+            System.err.println("Application crashed: " + e.getMessage());
+            e.printStackTrace();
+        }
+     
+    }
+}
